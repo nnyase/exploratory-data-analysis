fa592726ee551c538b80f16777aa4faa2c04cbad
Ndivhuwo Nyase <65094560+nnyase@users.noreply.github.com>
2023-03-01 20:15:08 +0200

    update

---
+++ Week 3/interactive/01-ia-exercise.r
@@ -0,0 +1,25 @@
+install.packages("plotly")
+library(dplyr)
+library(ggplot2)
+library(plotly)
+
+# load the diamonds dataset from the ggplot2 package
+data(diamonds)
+head(diamonds)
+
+# will try guess right plot if no extra info
+plot_ly(diamonds, x = ~cut)
+
+# or we can tell it what to plot
+plot_ly(diamonds, x = ~cut) %>% add_histogram()
+
+# basic style options
+plot_ly(diamonds, x = ~cut, color = "red")
+plot_ly(diamonds, x = ~cut, color = I("red"))
+plot_ly(diamonds, x = ~cut, color = I("red"), stroke = I("black"))
+plot_ly(diamonds, x = ~cut, color = I("red"), stroke = I("black"), span = I(5))
+plot_ly(diamonds, x = ~cut, color = I("red"), stroke = I("black"), span = I(5), alpha = I(0.2))
+
+# use the pipe to make the plot in the exercise
+diamonds %>%
+  plot_ly(x = ~x, y = ~price) %>% add_markers()
