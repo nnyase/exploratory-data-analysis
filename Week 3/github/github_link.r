050b9ea8615343ff55ad2c89e019a97ecd4d1246
Ndivhuwo Nyase <65094560+nnyase@users.noreply.github.com>
2023-03-08 16:20:54 +0200

    update

---
+++ Week 3/github/github_link.r
@@ -0,0 +1,11 @@
+library(usethis)
+
+git_sitrep()
+
+use_git_config(
+  user.name = 'your user github username',
+  user.email = 'your github email')
+
+create_github_token()
+
+edit_r_environ()
---
+++ Week 3/github/my_work.r
@@ -0,0 +1,1 @@
+read.csv('C:/Users/Stefan/OneDrive - University of Cape Town/Courses/STA5092Z - EDA/Lecture21_22_Git/Example/data/temperature.csv')
